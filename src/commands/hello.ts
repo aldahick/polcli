370b4c0f8601ff5429b25b9ce7cdf5cd58aebd29
Alex <alex@alexhicks.net>
2018-03-22 10:08:02 -0400

    Fix `yarn run lint`

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,29 +1,29 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from "@oclif/command";
 
 export default class Hello extends Command {
-  static description = 'describe the command here'
+  static description = "describe the command here";
 
   static examples = [
     `$ polcli hello
 hello world from ./src/hello.ts!
 `,
-  ]
+  ];
 
   static flags = {
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    force: flags.boolean({char: 'f'}),
-  }
+    name: flags.string({char: "n", description: "name to print"}),
+    force: flags.boolean({char: "f"}),
+  };
 
-  static args = [{name: 'file'}]
+  static args = [{name: "file"}];
 
   async run() {
-    const {args, flags} = this.parse(Hello)
+    const {args, flags} = this.parse(Hello);
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ${__filename}!`)
+    const name = flags.name || "world";
+    this.log(`hello ${name} from ${__filename}!`);
     if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+      this.log(`you input --force and --file: ${args.file}`);
     }
   }
 }
